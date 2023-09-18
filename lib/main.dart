0d9f39011d64b5f6350bba58ca0089cf08bc9139
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-18 18:14:03 +0200

    use streambuilder with auth and empty/ collection screen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:brainstorm_array/screens/auth.dart';
+import 'package:brainstorm_array/screens/collections.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,10 +21,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Brainstorm Array',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 4, 255, 180),
+        ),
         useMaterial3: true,
       ),
-      home: const Text('Hello world!'),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const CollectionsScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
