61ee1b0a0238c84e5ab4f9272384e92e9d1d0fff
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-19 14:02:14 +0200

    add basic screen/form to create collection

---
+++ lib/screens/new_collection.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:brainstorm_array/providers/collections.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class NewCollectionScreen extends HookConsumerWidget {
+  const NewCollectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final collectionFormKey = GlobalKey<FormState>();
+
+    var enteredTitle = '';
+
+    void submit() {
+      final isValid = collectionFormKey.currentState!.validate();
+
+      if (!isValid) {
+        return;
+      }
+
+      collectionFormKey.currentState!.save();
+
+      ref.read(collectionsProvider.notifier).addCollection(enteredTitle);
+
+      Navigator.of(context).pop();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new List'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Form(
+          key: collectionFormKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'List Name',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a list name';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  enteredTitle = newValue!;
+                },
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: submit,
+                    child: const Text('Add List'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
