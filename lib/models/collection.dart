2522e33c8ed972554db4f72d8dacb71818e1bf66
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-19 15:19:09 +0200

    add color to collection model

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class Collection {
   final String title;
   final Timestamp createdAt;
   final String uid;
+  final Color? color;
   final List<dynamic> array;
 
-  Collection(this.title, this.createdAt, this.uid, this.array);
+  Collection(this.title, this.createdAt, this.uid, this.color, this.array);
 }
