0e51df730c00e0b4a4cd04b024cb3147d7645a08
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-21 15:23:33 +0200

    add hero to animate collection into new_array_item_screen
    also I removed the Navigator.pop() when the form is submitted so multiple items can be added

--- lib/screens/new_array_item.dart
+++ lib/screens/new_array_item.dart
@@ -1,9 +1,11 @@
 import 'package:brainstorm_array/models/collection.dart';
 import 'package:brainstorm_array/providers/providers.dart';
+import 'package:brainstorm_array/widgets/array_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NewArrayItemScreen extends ConsumerWidget {
+class NewArrayItemScreen extends HookConsumerWidget {
   const NewArrayItemScreen({super.key, required this.collection});
 
   final Collection collection;
@@ -12,9 +14,11 @@ class NewArrayItemScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final arrayItemFormKey = GlobalKey<FormState>();
 
+    var collectionArray = useState(collection.array);
+
     var enteredName = '';
 
-    void submit() {
+    void submit() async {
       final isValid = arrayItemFormKey.currentState!.validate();
 
       if (!isValid) {
@@ -22,52 +26,94 @@ class NewArrayItemScreen extends ConsumerWidget {
       }
 
       arrayItemFormKey.currentState!.save();
-      ref
+
+      final itemUid = await ref
           .read(firestoreServiceProvider)
           .addItemToArray(collection.uid, enteredName);
 
-      Navigator.of(context).pop();
+      collectionArray.value = [
+        ...collectionArray.value,
+        {
+          'name': enteredName,
+          'uid': itemUid,
+        }
+      ];
+
+      arrayItemFormKey.currentState!.reset();
     }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Item'),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Form(
-            key: arrayItemFormKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    enteredName = newValue!;
-                  },
-                ),
-                const SizedBox(height: 12),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 10),
+        child: Container(
+          height: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: collection.color!),
+            color: collection.color!.withOpacity(0.2),
+          ),
+          child: Form(
+              key: arrayItemFormKey,
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    TextButton(
-                      child: const Text('Cancel'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
+                    Hero(
+                      tag: collection.uid,
+                      child: Material(
+                        type: MaterialType.transparency,
+                        child: Column(
+                          children: [
+                            Text(
+                              collection.title,
+                              style: TextStyle(
+                                color: collection.color,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+                            ArrayWidget(
+                              collectionUid: collection.uid,
+                              array: collectionArray.value,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        enteredName = newValue!;
                       },
                     ),
-                    ElevatedButton(
-                      onPressed: submit,
-                      child: const Text('Add Item'),
-                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: const Text('Cancel'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        ElevatedButton(
+                          onPressed: submit,
+                          child: const Text('Add Item'),
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
-            )),
+                ),
+              )),
+        ),
       ),
     );
   }
