d9cfe57efbd48018bd304ebcf69f523ae6f95e44
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-21 19:44:50 +0200

    add removeCollection and try / catch for echt request

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:brainstorm_array/models/collection.dart';
 import 'package:flutter/material.dart';
@@ -8,73 +10,135 @@ class FirestoreService {
       FirebaseFirestore.instance.collection('collections');
 
   Future<List<Collection>> getCollections() async {
-    final snapshot = await collectionsCollection.get();
-    return snapshot.docs.map((doc) {
-      return Collection(
-        doc['title'],
-        doc['createdAt'],
-        doc.id,
-        Color(doc['color']),
-        doc['array'],
-      );
-    }).toList();
+    try {
+      final snapshot = await collectionsCollection.get();
+      return snapshot.docs.map((doc) {
+        return Collection(
+          doc['title'],
+          doc['createdAt'],
+          doc.id,
+          Color(doc['color']),
+          doc['array'],
+        );
+      }).toList();
+    } on FirebaseException catch (error) {
+      print("Error fetching collections: $error");
+
+      return Future.error("Failed to fetch collections");
+    }
   }
 
   Future<Collection> getCollection(String collectionUid) async {
-    final snapshot = await collectionsCollection.doc(collectionUid).get();
-    return Collection(
-      snapshot['title'],
-      snapshot['createdAt'],
-      snapshot.id,
-      Color(snapshot['color']),
-      snapshot['array'],
-    );
+    try {
+      final snapshot = await collectionsCollection.doc(collectionUid).get();
+      return Collection(
+        snapshot['title'],
+        snapshot['createdAt'],
+        snapshot.id,
+        Color(snapshot['color']),
+        snapshot['array'],
+      );
+    } on FirebaseException catch (error) {
+      print("Error fetching collection: $error");
+
+      return Future.error("Failed to fetch collection");
+    }
   }
 
   Future<Collection> addCollection(Map<String, dynamic> newCollection) async {
-    final collectionReference = await collectionsCollection.add({
-      'title': newCollection['title'],
-      'createdAt': Timestamp.now(),
-      'color': newCollection['color'].value,
-      'array': [],
-    });
+    try {
+      final collectionReference = await collectionsCollection.add({
+        'title': newCollection['title'],
+        'createdAt': Timestamp.now(),
+        'color': newCollection['color'].value,
+        'array': [],
+      });
 
-    return Collection(
-      newCollection['title'],
-      Timestamp.now(),
-      collectionReference.id,
-      newCollection['color'],
-      [],
-    );
+      return Collection(
+        newCollection['title'],
+        Timestamp.now(),
+        collectionReference.id,
+        newCollection['color'],
+        [],
+      );
+    } on FirebaseException catch (error) {
+      print("Error creating collection: $error");
+
+      return Future.error("Failed to create collection");
+    }
   }
 
-  Future<String> addItemToArray(String collectionUid, String item) async {
-    final collection = await getCollection(collectionUid);
+  Future<Collection> editCollection(
+      String collectionUid, Map<String, dynamic> updatedCollection) async {
+    try {
+      await collectionsCollection.doc(collectionUid).update({
+        'title': updatedCollection['title'],
+        'color': updatedCollection['color'].value,
+      });
 
-    var uuid = const Uuid().v4();
+      return Collection(
+        updatedCollection['title'],
+        Timestamp.now(),
+        collectionUid,
+        updatedCollection['color'],
+        [],
+      );
+    } on FirebaseException catch (error) {
+      print("Error editing collection: $error");
 
-    collectionsCollection.doc(collectionUid).update({
-      'array': [
-        ...collection.array,
-        {
-          'name': item,
-          'uid': uuid,
-        }
-      ],
-    });
+      return Future.error("Failed to edit collection");
+    }
+  }
 
-    return uuid;
+  Future<void> removeCollection(String collectionUid) async {
+    try {
+      await collectionsCollection.doc(collectionUid).delete();
+    } on FirebaseException catch (error) {
+      print("Error deleting collection: $error");
+
+      return Future.error("Failed to delete collection");
+    }
+  }
+
+  FutureOr<String> addItemToArray(String collectionUid, String item) async {
+    try {
+      final collection = await getCollection(collectionUid);
+
+      var uuid = const Uuid().v4();
+
+      collectionsCollection.doc(collectionUid).update({
+        'array': [
+          ...collection.array,
+          {
+            'name': item,
+            'uid': uuid,
+          }
+        ],
+      });
+
+      return uuid;
+    } on FirebaseException catch (error) {
+      print("Error add item to array: $error");
+
+      return Future.error("Failed to add item");
+    }
   }
 
   Future<void> removeItemFromArray(
       String collectionUid, Map<String, dynamic> item) async {
-    final collection = await getCollection(collectionUid);
+    try {
+      final collection = await getCollection(collectionUid);
 
-    final updatedArray =
-        collection.array.where((element) => element['uid'] != item['uid']);
+      final updatedArray =
+          collection.array.where((element) => element['uid'] != item['uid']);
 
-    collectionsCollection.doc(collectionUid).update({
-      'array': updatedArray.toList(),
-    });
+      collectionsCollection.doc(collectionUid).update({
+        'array': updatedArray.toList(),
+      });
+    } on FirebaseException catch (error) {
+      print("Error removing item to array: $error");
+
+      return Future.error("Failed to remove item");
+    }
   }
 }
