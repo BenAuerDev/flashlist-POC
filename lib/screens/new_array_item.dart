d1809d895d07faa106db2d8958298df61fac1dd8
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-09-21 20:49:11 +0200

    replace padding widget with container-margin

--- lib/screens/new_array_item.dart
+++ lib/screens/new_array_item.dart
@@ -1,6 +1,7 @@
 import 'package:brainstorm_array/models/collection.dart';
 import 'package:brainstorm_array/providers/providers.dart';
 import 'package:brainstorm_array/widgets/array_widget.dart';
+import 'package:brainstorm_array/widgets/collection_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -46,59 +47,53 @@ class NewArrayItemScreen extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('Add Item'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 10),
-        child: Container(
-          height: double.infinity,
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          decoration: BoxDecoration(
-            border: Border.all(color: collection.color!),
-            color: collection.color!.withOpacity(0.2),
-          ),
-          child: Form(
-              key: arrayItemFormKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                          ),
-                        ),
-                      ),
-                    ),
-                    TextFormField(
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                      onSaved: (newValue) {
-                        enteredName = newValue!;
-                      },
-                    ),
-                    const SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          child: const Text('Cancel'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        ElevatedButton(
-                          onPressed: submit,
-                          child: const Text('Add Item'),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )),
+      body: Container(
+        margin: const EdgeInsets.all(4),
+        height: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        decoration: BoxDecoration(
+          border: Border.all(color: collection.color!),
+          color: collection.color!.withOpacity(0.2),
         ),
+        child: Form(
+            key: arrayItemFormKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
                   Hero(
                     tag: collection.uid,
                     child: CollectionWidget(collection: collection),
                   ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      enteredName = newValue!;
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: const Text('Cancel'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      ElevatedButton(
+                        onPressed: submit,
+                        child: const Text('Add Item'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )),
       ),
     );
   }
