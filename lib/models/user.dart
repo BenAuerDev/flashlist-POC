8802898b5b41656553e2a1769e1efdf490a7e9f6
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-11 15:05:42 +0200

    add custom-user model to simplify handling user-data

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class CustomUser {
+  final String email;
+  final String username;
+  final String uid;
+  final String? imageUrl;
+
+  CustomUser(
+    this.email,
+    this.username,
+    this.uid,
+    this.imageUrl,
+  );
+}
