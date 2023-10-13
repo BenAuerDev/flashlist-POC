caa1a8931225bc69f86818731fd219d678d5f528
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-13 14:44:15 +0200

    wrap stack in sizedBox
    this tries to achieve, a clean alignment to have all 3 "group header" (avatar-group, title, menu-button) be in kind of a clean line

--- lib/widgets/group/group_widget.dart
+++ lib/widgets/group/group_widget.dart
@@ -16,31 +16,37 @@ class GroupWidget extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Center(
-                  child: Text(
-                    group.title,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: group.color,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+            SizedBox(
+              height: 38,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  Center(
+                    child: Text(
+                      group.title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: group.color,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    if (group.permissions['editors'].isEmpty)
-                      const SizedBox(width: 50),
-                    if (group.permissions['editors'].isNotEmpty)
-                      AvatarGroup(group: group),
-                    GroupMenu(group: group),
-                  ],
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (group.permissions['editors'].isEmpty)
+                        const SizedBox(width: 50),
+                      if (group.permissions['editors'].isNotEmpty)
+                        AvatarGroup(group: group),
+                      GroupMenu(group: group),
+                    ],
+                  ),
+                ],
+              ),
             ),
+            const SizedBox(height: 8),
             GroupBody(groupUid: group.uid),
           ],
         ),
