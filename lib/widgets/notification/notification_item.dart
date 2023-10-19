1b5f0cff8c03b042fb54b5b705d65e3ff9d730a1
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-19 13:15:14 +0200

    extract notification item into it's own widget for simplicty

---
+++ lib/widgets/notification/notification_item.dart
@@ -0,0 +1,37 @@
+import 'package:brainstorm_array/models/notification.dart';
+import 'package:brainstorm_array/utils/context_retriever.dart';
+import 'package:flutter/material.dart';
+
+class NotificationItem extends StatelessWidget {
+  const NotificationItem({super.key, required this.notification});
+
+  final UserNotification notification;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        notification.isRead = false;
+        Navigator.of(context).pop();
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 4),
+        color: notification.isRead
+            ? Colors.transparent
+            : retrieveColorScheme(context).primaryContainer.withOpacity(0.2),
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 24,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(notification.message),
+          ],
+        ),
+      ),
+    );
+  }
+}
