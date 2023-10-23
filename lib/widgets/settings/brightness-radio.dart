457c2771153472455b2797472543a573c6da7ced
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-23 15:23:21 +0200

    create brightness-radio
    A list tile to be inserted into the list-view in settings.
    
    holding
    - an Icon (depending on them)
    - text: 'Brightness Theme'
    - toggle-buttons (Light, Dark, System)
    
    it isolates the whole logic with adaptive theme to it's file. this way the settings-screen can stay a very simple widget and each setting (if unrelated) holds only the logic for 1 setting item

---
+++ lib/widgets/settings/brightness-radio.dart
@@ -0,0 +1,72 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class BrightnessRadio extends HookWidget {
+  const BrightnessRadio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final currentThemeMode = AdaptiveTheme.of(context).mode;
+
+    var selectedModes = useState<List<bool>>([false, false, true]);
+
+    useEffect(() {
+      if (currentThemeMode == AdaptiveThemeMode.light) {
+        selectedModes.value = [true, false, false];
+      } else if (currentThemeMode == AdaptiveThemeMode.dark) {
+        selectedModes.value = [false, true, false];
+      } else {
+        selectedModes.value = [false, false, true];
+      }
+      return;
+    }, [AdaptiveTheme.of(context).mode]);
+
+    return ListTile(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              // Icon
+              if (currentThemeMode == AdaptiveThemeMode.light)
+                const Icon(Icons.light_mode),
+              if (currentThemeMode == AdaptiveThemeMode.dark)
+                const Icon(Icons.dark_mode),
+              if (currentThemeMode == AdaptiveThemeMode.system)
+                const Icon(Icons.settings),
+              const SizedBox(width: 10),
+
+              const Text('Brightness Theme')
+            ],
+          ),
+          ToggleButtons(
+            direction: Axis.horizontal,
+            isSelected: selectedModes.value,
+            onPressed: (index) {
+              switch (index) {
+                case 0:
+                  AdaptiveTheme.of(context).setLight();
+                  selectedModes.value = [true, false, false];
+                  break;
+                case 1:
+                  AdaptiveTheme.of(context).setDark();
+                  selectedModes.value = [false, true, false];
+                  break;
+                default:
+                  AdaptiveTheme.of(context).setSystem();
+                  selectedModes.value = [false, false, true];
+                  break;
+              }
+            },
+            children: const [
+              Text('Light'),
+              Text('Dark'),
+              Text('System'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
