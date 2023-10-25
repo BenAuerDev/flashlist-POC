2a12fbcfa5f3513a165c252fe40dfd465f47eb6b
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-25 16:09:52 +0200

    update user-notification model

--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -1,11 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserNotification {
-  final String message;
+  final String title;
+  final String body;
   final String uid;
+  final Map<String, dynamic>? data;
+  final Timestamp createdAt;
   bool isRead;
 
   UserNotification(
-    this.message,
+    this.title,
+    this.body,
     this.uid,
+    this.data,
+    this.createdAt,
     this.isRead,
   );
 }
