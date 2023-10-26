480d675487ad60940f251ade5a194b556053932f
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-10-26 20:53:19 +0200

    change alignment of red-circle on notification badge

--- lib/widgets/notification/notification_badge.dart
+++ lib/widgets/notification/notification_badge.dart
@@ -1,5 +1,5 @@
 import 'package:flash_list/providers/providers.dart';
-import 'package:flash_list/screens/notifications.dart';
+import 'package:flash_list/utils/context_retriever.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,57 +8,60 @@ class NotificationBadge extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Stack(
-      children: [
-        IconButton(
-          icon: const Icon(Icons.notifications),
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const NotificationsScreen(),
-              ),
-            );
-          },
-        ),
-        StreamBuilder(
-          stream: ref
-              .watch(firestoreServiceProvider)
-              .userUnreadNotificationsCountStream(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const SizedBox();
-            } else if (snapshot.hasError) {
-              return const SizedBox();
-            } else if (!snapshot.hasData || snapshot.data == null) {
-              return const SizedBox();
-            }
+    return Container(
+      width: 40,
+      height: 40,
+      alignment: Alignment.center,
+      child: Stack(
+        children: [
+          Icon(
+            Icons.notifications,
+            color: retrieveColorScheme(context).onBackground,
+          ),
+          StreamBuilder(
+            stream: ref
+                .watch(firestoreServiceProvider)
+                .userUnreadNotificationsCountStream(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox();
+              } else if (snapshot.hasError) {
+                return const SizedBox();
+              } else if (!snapshot.hasData || snapshot.data == null) {
+                return const SizedBox();
+              }
 
-            final count = snapshot.data;
+              final count = snapshot.data;
 
-            if (count != null && count > 0) {
-              return Positioned(
-                right: 8,
-                top: 6,
-                child: Container(
-                  padding: const EdgeInsets.all(3),
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red,
-                  ),
-                  child: Text(
-                    count.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
+              if (count != null && count > 0) {
+                return Positioned(
+                  right: 0,
+                  bottom: 6,
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 15,
+                    padding: const EdgeInsets.all(3),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.red,
+                    ),
+                    child: Text(
+                      count.toString(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              );
-            } else {
-              return const SizedBox();
-            }
-          },
-        )
-      ],
+                );
+              } else {
+                return const SizedBox();
+              }
+            },
+          )
+        ],
+      ),
     );
   }
 }
