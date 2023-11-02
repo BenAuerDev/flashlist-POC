d36476f2e8bfe547567b29fc86a95bbfb997d982
BenAuerDev <136239531+BenAuerDev@users.noreply.github.com>
2023-11-02 12:06:19 +0100

    add retriever for AppLocalizations

--- lib/utils/context_retriever.dart
+++ lib/utils/context_retriever.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 ThemeData retrieveTheme(context) {
   return Theme.of(context);
@@ -10,4 +11,8 @@ TextTheme retrieveTextTheme(context) {
 
 ColorScheme retrieveColorScheme(context) {
   return retrieveTheme(context).colorScheme;
+}
+
+AppLocalizations retrieveAppLocalizations(context) {
+  return AppLocalizations.of(context)!;
 }
