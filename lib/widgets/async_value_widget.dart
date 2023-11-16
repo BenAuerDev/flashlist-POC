23ef7dfbf7322ea69ee69210ec732694aa77629f
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2023-11-16 16:43:55 +0100

    add AsyncValueWidget

---
+++ lib/widgets/async_value_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  const AsyncValueWidget({super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T data) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (e, st) => Center(child: Text(e.toString())),
+    );
+  }
+}
