835e46444859a738d5e2e49eee1600830909aa55
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2023-11-16 17:58:47 +0100

    add custom loading state to avatar group

--- lib/widgets/avatar_group.dart
+++ lib/widgets/avatar_group.dart
@@ -7,6 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shimmer/shimmer.dart';
 
 final currentUser = FirebaseAuth.instance.currentUser;
 
@@ -48,58 +49,91 @@ class AvatarGroup extends HookConsumerWidget {
       }
     }
 
+    final groupUsersValue = ref.watch(groupUsersProvider(group.uid));
+
     return AnimatedBuilder(
       animation: animationController,
       builder: (BuildContext context, Widget? child) {
-        return ref.watch(groupUsersProvider(group.uid)).when(
-              data: (groupUsers) {
-                final List<CustomUser> usersWithoutCurrent = groupUsers
-                    .where((user) => user.uid != currentUser!.uid)
-                    .toList();
+        return groupUsersValue.when(
+          data: (groupUsers) {
+            final List<CustomUser> usersWithoutCurrent = groupUsers
+                .where((user) => user.uid != currentUser!.uid)
+                .toList();
 
-                if (usersWithoutCurrent.isEmpty) {
-                  return const SizedBox();
-                }
+            if (usersWithoutCurrent.isEmpty) {
+              return const SizedBox();
+            }
 
-                return GestureDetector(
-                  onTap: group.permissions.editors.length > 1
-                      ? expandTemporarily
-                      : null,
-                  child: Row(
-                    textDirection: TextDirection.rtl,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      for (var user in usersWithoutCurrent.reversed)
-                        Align(
-                          alignment: isExpanded.value
-                              ? Alignment.bottomRight
-                              : Alignment.bottomRight,
-                          widthFactor: user == usersWithoutCurrent.first
-                              ? 0.9
-                              : animationController.value,
-                          child: SlideFadeTransition(
-                            index: usersWithoutCurrent.indexOf(user),
-                            position: animationController.value,
-                            animationController: animationController,
-                            direction: isExpanded.value ? 1 : -1,
-                            child: CircleAvatar(
-                              radius: Sizes.p16,
-                              backgroundImage: NetworkImage(
-                                user.imageUrl ??
-                                    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-                              ),
-                            ),
+            return GestureDetector(
+              onTap: group.permissions.editors.length > 1
+                  ? expandTemporarily
+                  : null,
+              child: Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (var user in usersWithoutCurrent.reversed)
+                    Align(
+                      alignment: isExpanded.value
+                          ? Alignment.bottomRight
+                          : Alignment.bottomRight,
+                      widthFactor: user == usersWithoutCurrent.first
+                          ? 0.9
+                          : animationController.value,
+                      child: SlideFadeTransition(
+                        index: usersWithoutCurrent.indexOf(user),
+                        position: animationController.value,
+                        animationController: animationController,
+                        direction: isExpanded.value ? 1 : -1,
+                        child: CircleAvatar(
+                          radius: Sizes.p16,
+                          backgroundImage: NetworkImage(
+                            user.imageUrl ??
+                                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
                           ),
                         ),
-                    ],
-                  ),
-                );
-              },
-              // TODO: add special loading case with shimmer
-              loading: () => const SizedBox(),
-              error: (error, stackTrace) => const SizedBox(),
+                      ),
+                    ),
+                ],
+              ),
             );
+          },
+          loading: () {
+            // TODO: think about how to make this file smaller
+            final editors = group.permissions.editors;
+
+            return Shimmer(
+              gradient: LinearGradient(colors: [
+                Colors.grey.withOpacity(0.2),
+                Colors.black12.withOpacity(0.2),
+              ]),
+              child: Row(
+                textDirection: TextDirection.rtl,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (var i = 0; i < editors.length; i++)
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      widthFactor: i == editors.length - 1 ? 1 : 0.3,
+                      child: Container(
+                        width: Sizes.p32,
+                        height: Sizes.p32,
+                        decoration: BoxDecoration(
+                          color: group.color,
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: const SizedBox(),
+                      ),
+                    )
+                ],
+              ),
+            );
+          },
+          error: (error, stackTrace) => const SizedBox(),
+        );
       },
     );
   }
