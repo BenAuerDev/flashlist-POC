90b77cdca608d294fcbaf46b0002e38c34ac285e
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2023-12-13 23:44:38 +0100

    add temporary notes tab

---
+++ lib/screens/tabs/notes_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class NotesList extends HookConsumerWidget {
+  const NotesList({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Center(child: Text("Notes coming soon...."));
+  }
+}
